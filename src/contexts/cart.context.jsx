aea0ab78ffb7c26c27f97d2eb843b68d2f233bec
Bryan Jones <thebeard@me.com>
2023-05-16 16:46:37 -0700

    adding cart context and shopping icon as well as cart dropdown

---
+++ src/contexts/cart.context.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+import { Value } from "sass";
+
+
+export const CartContext = createContext({
+  isCartOpen: false,
+  setIsCartOpem: () => {}
+});
+
+
+export const CartProvider = ({children}) => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const value = { isCartOpen, setIsCartOpen };
+  
+  return (
+    <CartContext.Provider value={value}>{ children }</CartContext.Provider>
+  )
+}
